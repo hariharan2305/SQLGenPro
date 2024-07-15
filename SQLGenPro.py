edeba687abf070899b1f364569716ff594301c70
HD's Mac <hariharan_23@Apples-MacBook-Pro.local>
2024-07-15 14:16:24 +0530

    tested the snowflake connector and rest of the initial setup

---
+++ SQLGenPro.py
@@ -0,0 +1,10 @@
+import os 
+import pandas as pd 
+import numpy as np
+import streamlit as st
+import sqlparse
+from collections import OrderedDict, Counter
+from github import Github
+
+
+print("Hello World!!")
